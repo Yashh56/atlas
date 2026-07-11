67e35a972d57cc96831565c6e29aed4d532e8cdd
Yash <yashsaini18166@gmail.com>
2026-07-11 22:32:36 +0530

    feat: added .env in .gitignore and installed godotenv

--- cmd/atlas/main.go
+++ cmd/atlas/main.go
@@ -4,9 +4,14 @@ import (
 	"os"
 
 	"github.com/Yashh56/atlas/cmd/atlas/cmd"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Best-effort load from .env in the current working directory.
+	// We ignore the error because .env is optional.
+	_ = godotenv.Load()
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
